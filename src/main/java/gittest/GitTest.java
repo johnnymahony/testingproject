e87c616dfe8866b444f048ab828d57d8f4070d97
schotjan <johnnymahony00000@gmail.com>
2015-07-25 19:59:18 +0200

    Testing a commit of project files...

---
+++ src/main/java/gittest/GitTest.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package gittest;
+
+/**
+ * @author johnnymahony
+ *
+ */
+public final class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+	}
+
+}
